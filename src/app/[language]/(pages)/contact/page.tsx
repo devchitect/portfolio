21387935ed4a7b3b969c8f816be4cca4218bca8f
Dzung Nguyen Tr <mr.dzung.2k@gmail.com>
2023-12-18 22:24:55 +0700

    fix

--- src/app/[language]/(pages)/contact/page.tsx
+++ src/app/[language]/(pages)/contact/page.tsx
@@ -1,11 +1,6 @@
 'use client'
 
 import { useRef } from "react"
-import useIsomorphicLayoutEffect from "@/component/gsap-helper/isomorphic-effect";
-import { gsap } from 'gsap/dist/gsap';
-import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-
-gsap.registerPlugin(ScrollTrigger);
 
 export default function Contact() {
     
